92658a3c71b0814f68c370fbaf4905744e8b363c
vietvb <vietvb@epi.com.vn>
2019-02-15 14:51:06 +0700

    Add project files.

---
+++ TemplateSliderDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TemplateSliderDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
